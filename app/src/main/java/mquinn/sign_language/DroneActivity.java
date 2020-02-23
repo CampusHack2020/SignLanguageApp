7db3d364943c2112590f4bbbf9c823f76ec2c9f1
as14g18 <as14g18@soton.ac.uk>
2020-02-23 03:41:31 +0000

    Drone flies on app button press

--- app/src/main/java/mquinn/sign_language/DroneActivity.java
+++ app/src/main/java/mquinn/sign_language/DroneActivity.java
@@ -1,17 +1,49 @@
 package mquinn.sign_language;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
+import android.widget.Toast;
+
+import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler;
+import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
+import com.github.hiteshsondhi88.libffmpeg.FFmpegLoadBinaryResponseHandler;
+import com.github.hiteshsondhi88.libffmpeg.LoadBinaryResponseHandler;
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class DroneActivity extends Activity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.drone_layout);  //TODO
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        DroneController dc = new DroneController();
+
+        try {
+
+            dc.connect("192.168.10.1", 8889);
+            dc.takeOff();
+            dc.land();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
